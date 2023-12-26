b15fc5316da0c3493b5330508afd08fc6d653f78
Harivansh Bhardwaj <65931691+harivanshx@users.noreply.github.com>
2023-12-26 22:48:29 +0530

    new js

--- js/main.js
+++ js/main.js
@@ -1,22 +1,26 @@
-console.log("hello world")
-console.log(typeof "harivansh")
-console.log(typeof 24)
-console.log(typeof true)
-console.log(typeof false)
-console.log(typeof undefined)
-console.log(typeof 22.500)
-console.log(typeof null)
+// console.log("hello world")
+// console.log(typeof "harivansh")
+// console.log(typeof 24)
+// console.log(typeof true)
+// console.log(typeof false)
+// console.log(typeof undefined)
+// console.log(typeof 22.500)
+// console.log(typeof null)
 
 
-const account_id =5500
-// let Email ="harovansh@gmail.com"
-var password ="123456"
-// account_id=2
-Email="32"
-password="12"
-console.log(account_id)
-console.log(Email)
-console.log(password)
-/* haan bhai ye hai* */
+// const account_id =5500
+// // let Email ="harovansh@gmail.com"
+// var password ="123456"
+// // account_id=2
+// Email="32"
+// password="12"
+// console.log(account_id)
+// console.log(Email)
+// console.log(password)
+// /* haan bhai ye hai* */
+
+
+
+
 
 
